75a05374460e258b152f6074bf14249d5d53a1a6
Matheus Rodrigues dos Santos <matheus.rodrisantos@outlook.com>
2024-06-13 15:52:55 -0300

    iniciando os jogos

---
+++ src/array/RemoveSpace.php
@@ -0,0 +1,22 @@
+<?php 
+
+/** this algorithms will to remove to space  in multidimensional arrays */
+# exemple 1;
+$array=[
+    "Computador "=>
+    [
+        "Placa mãe", 
+        "Memória", 
+        "Teclado ", 
+    ],
+    "Notebook"=>
+    [
+        "Dell  ",
+        " Acer ", 
+        "Positivo "
+    ], 
+    "Caneca ", 
+    "Monitor"     
+];
+
+
